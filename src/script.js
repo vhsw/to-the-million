5ee68566d8da4ec3af2ff563ff823c3b5eabaf06
Alexey Dubrov <nevermind1025@gmail.com>
2021-08-01 18:33:38 +0300

    add searchParams, format

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ const dt = 0.5;
 
 let amount;
 let currency;
-let daysDelta;
+let days;
 let historyPrice;
 let currentPrice;
 let multiplier;
@@ -12,13 +12,13 @@ let sum;
 let time;
 
 function showLoader () {
-    $("#loader").show()
-    $("#sum").hide()
+    $("#loader").show();
+    $("#sum").hide();
 }
 
 function hideLoader () {
-    $("#sum").show()
-    $("#loader").hide()
+    $("#sum").show();
+    $("#loader").hide();
 }
 
 $("#amount-select").change(function () {
@@ -29,68 +29,90 @@ $("#amount-select").change(function () {
 $("#currency-select").change(function () {
     currency = this.value;
     update();
-
 });
 $("#days-delta-select").change(function () {
-    daysDelta = this.value;
+    days = this.value;
     update();
 });
 
 async function fetchHistoryPrice (currency, date) {
     let price;
-    await $.getJSON(`${apiUrl}/coins/${currency}/history`, { date, localization: false }).done(
-        (data) => {
-            price = data.market_data.current_price.usd
-        });
+    await $.getJSON(`${apiUrl}/coins/${currency}/history`, {
+        date,
+        localization: false,
+    }).done((data) => {
+        price = data.market_data.current_price.usd;
+    });
     return price;
 }
 async function fetchCurrentPrice (coin) {
     let price;
-    await $.getJSON(`${apiUrl}/simple/price`, { ids: coin, vs_currencies: "usd" },).done(
-        (data) => {
-            price = data[coin].usd
-        })
+    await $.getJSON(`${apiUrl}/simple/price`, {
+        ids: coin,
+        vs_currencies: "usd",
+    }).done((data) => {
+        price = data[coin].usd;
+    });
     return price;
 }
 
-
 async function update () {
     time = 0;
-    const date = moment().subtract(daysDelta, 'days').format("DD-MM-YYYY");
+    const date = moment().subtract(days, "days").format("DD-MM-YYYY");
 
     try {
         showLoader();
         historyPrice = await fetchHistoryPrice(currency, date);
         currentPrice = await fetchCurrentPrice(currency);
         multiplier = historyPrice / currentPrice;
-        perSec = (historyPrice - currentPrice) / currentPrice / (daysDelta * 24 * 60 * 60);
+        perSec =
+            (historyPrice - currentPrice) / currentPrice / (days * 24 * 60 * 60);
         animateSum();
-    }
-    finally {
+    } finally {
         hideLoader();
     }
     updateCalc();
 }
 
+function updateSearchParams () {
+    var searchParams = new URLSearchParams(window.location.search);
+    searchParams.set("amount", amount);
+    searchParams.set("currency", currency);
+    searchParams.set("days", days);
+    var newRelativePathQuery =
+        window.location.pathname + "?" + searchParams.toString();
+    history.pushState(null, "", newRelativePathQuery);
+}
+
 function updateCalc () {
     const usdStart = amount * multiplier;
     const roi = (currentPrice - historyPrice) / historyPrice;
-    $(".currency").each(function () { $(this).text(currency) })
-    $(".date-start").each(function () { $(this).text(moment().subtract(daysDelta, 'days').calendar()) });
+    $(".currency").each(function () {
+        $(this).text(currency);
+    });
+    $(".date-start").each(function () {
+        $(this).text(moment().subtract(days, "days").calendar());
+    });
     $("#date-end").text(moment().calendar());
     $("#cost-start").text(formatCurrency(historyPrice));
     $("#cost-end").text(formatCurrency(currentPrice));
     $("#usd-amount-start").text(formatCurrency(usdStart));
-    $(".usd-amount-end").each(function () { $(this).text(formatCurrency(amount)) });
-    $(".coin-amount").each(function () { $(this).text(Number(usdStart / historyPrice).toLocaleString()) });
+    $(".usd-amount-end").each(function () {
+        $(this).text(formatCurrency(amount));
+    });
+    $(".coin-amount").each(function () {
+        $(this).text(Number(usdStart / historyPrice).toLocaleString());
+    });
     $("#roi").text(Number(roi).toLocaleString("en", { style: "percent" }));
     $("#usd-amount-roi").text(formatCurrency(amount / multiplier));
+    updateSearchParams();
 }
 
-
-
 function formatCurrency (num) {
-    return Number(num).toLocaleString("en", { style: 'currency', currency: 'USD' });
+    return Number(num).toLocaleString("en", {
+        style: "currency",
+        currency: "USD",
+    });
 }
 
 function animateSum () {
@@ -101,15 +123,18 @@ function animateSum () {
 }
 
 $(function () {
-    amount = $("#amount-select option:selected").val();
-    currency = $("#currency-select option:selected").val();
-    daysDelta = $("#days-delta-select option:selected").val();
-
+    const searchParams = new URLSearchParams(window.location.search);
+    amount = searchParams.get("amount") || 1000000;
+    currency = searchParams.get("currency") || "bitcoin";
+    days = searchParams.get("days") || 90;
+    $("#amount-select").val(amount);
+    $("#currency-select").val(currency);
+    $("#days-delta-select").val(days);
     update();
     setInterval(animateSum, dt * 1000);
     const minDistance = Math.min($(document).width(), $(document).height()) / 4;
     Particles.init({
-        selector: '.background',
+        selector: ".background",
         connectParticles: true,
         color: "#5196ca",
         minDistance: minDistance,
